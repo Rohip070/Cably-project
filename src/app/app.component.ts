29b5cd6bc30c3fa8ea2bdf3f2d16335bb4fb9081
unknown <oluwasegunsanusi4@gmail.com>
2025-03-28 13:33:29 -0700

    product-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'project';
 }
+
+
+
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { SecondSectionComponent } from "../second-section/second-section.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, SecondSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
