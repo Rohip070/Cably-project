d3dff7164f78cace454f6369408ca321bcfc1c92
unknown <oluwasegunsanusi4@gmail.com>
2025-03-29 22:57:25 -0700

    next-section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,4 +12,5 @@ export const routes: Routes = [
         component: HomeComponent
     },
 
+
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SecondSectionComponent } from "../second-section/second-section.component";
+import { ThirdSectionComponent } from "../third-section/third-section.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, SecondSectionComponent],
+  imports: [NavbarComponent, SecondSectionComponent, ThirdSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
