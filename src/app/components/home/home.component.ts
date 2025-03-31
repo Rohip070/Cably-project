7dd6893b8206daeeeeb4835f91f795f8265814bb
unknown <oluwasegunsanusi4@gmail.com>
2025-03-31 10:58:45 -0700

    footer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SecondSectionComponent } from "../second-section/second-section.component";
 import { ThirdSectionComponent } from "../third-section/third-section.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, SecondSectionComponent, ThirdSectionComponent],
+  imports: [NavbarComponent, SecondSectionComponent, ThirdSectionComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
